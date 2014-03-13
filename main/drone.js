a7e5021649d47aaf3bb25c98ad4542f2c5d8e54d
dom <dominic.wyler@zuehlke.com>
2014-03-13 09:23:38 +0100

    added drone client example code

---
+++ main/drone.js
@@ -0,0 +1,27 @@
+﻿var drone = require('ar-drone');
+var client  = drone.createClient();
+
+client.takeoff();
+
+client
+  .after(5000, function() {
+    this.clockwise(0.5);
+  })
+  .after(3000, function() {
+    this.stop();
+    this.land();
+  });
+
+client
+    .after(1000, function() {
+        this.takeoff();
+    })
+    .after(2000, function() {
+        this.up(0.2);
+    })
+    .after(2000, function() {
+        this.stop();
+        this.land();
+    });
+
+console.log('done');
